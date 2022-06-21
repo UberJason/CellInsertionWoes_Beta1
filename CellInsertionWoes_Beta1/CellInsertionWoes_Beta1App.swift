0dbf96a7cc2c3cba08c72f8312f4ff1a6d9b8b04
Jason Ji <uberjason@gmail.com>
2022-06-20 20:44:11 -0400

    Initial Commit

---
+++ CellInsertionWoes_Beta1/CellInsertionWoes_Beta1App.swift
@@ -0,0 +1,17 @@
+//
+//  CellInsertionWoes_Beta1App.swift
+//  CellInsertionWoes_Beta1
+//
+//  Created by Jason Ji on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CellInsertionWoes_Beta1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
